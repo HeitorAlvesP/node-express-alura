f75056d901448457729311b5a47d83361eac5db1
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-02-15 14:20:37 -0300

    create routes

--- express_mongo/server.js
+++ express_mongo/server.js
@@ -1,10 +1,18 @@
 import http from "http";
 
+const PORT = 3000
+
+const rotas = {
+    "/": "Curso de Node.js",
+    "/livros": "Aqui fica o nome dos livros",
+    "/autores": "Aqui fica o nome dos autores"
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("curso de node.js")
+    res.end(rotas[req.url])
 })
 
-server.listen(3000,() => {
+server.listen(PORT,() => {
     console.log('Porta 3000 ok')
 })
