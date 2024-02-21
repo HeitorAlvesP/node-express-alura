0fd973b2f47fd0e3716241280d3d83d77903ee2e
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-02-21 16:11:32 -0300

    crud refactoring

--- express_mongo/src/controllers/livroController.js
+++ express_mongo/src/controllers/livroController.js
@@ -3,18 +3,52 @@ import livro from "../models/Livro.js";
 class LivroController {
     
     static async listarLivros (req, res) {
-        const listaLivros = await livro.find({});
-        res.status(200).json(listaLivros)
+        try {
+            const listaLivros = await livro.find({});
+            res.status(200).json(listaLivros)
+        } catch (error) {
+            res.status(501).json({  message: `${error.message} - falha na requisição ` })
+        }
     }
 
     static async cadastrarLivros (req, res) {
         try {
             const novoLivro = await livro.create(req.body);
-            req.status(201).json({ message: "criado com sucesso", livro: novoLivro})
+            res.status(201).json({ message: "criado com sucesso", livro: novoLivro})
         } catch (error) {
             res.status(500).json({ message: `${error.message} - falha ao cadastrar o livro` })
         }
         
+    }
+
+    static async buscarLivro (req, res) {
+        try {
+            const id = req.params.id;
+            const Livro = await livro.findById(id)  
+            res.status(200).json(Livro)
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - falha ao exibir o livro` })
+        }
+    }
+
+    static async atualizarLivro (req, res) {
+        try {
+            const id = req.params.id
+            const Livro = await livro.findByIdAndUpdate(id, req.body)
+            res.status(200).json({ message: "Livro atualizado com sucesso" })
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - falha ao atualizar livro` })
+        }
+    }
+
+    static async deletarLivro (req, res) {
+        try {
+            const id = req.params.id;
+            const Livro = await livro.findByIdAndDelete(id)
+            res.status(200).json({ message: "Livro deletado com sucesso" })
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - falha ao deletar o livro` })
+        }
     }
 
 };
--- express_mongo/src/routes/livrosRoutes.js
+++ express_mongo/src/routes/livrosRoutes.js
@@ -4,5 +4,9 @@ import LivroController from "../controllers/livroController.js";
 const routes = express.Router()
 
 routes.get("/livros", LivroController.listarLivros);
+routes.get("/livros/:id", LivroController.buscarLivro);
+routes.post("/livros", LivroController.cadastrarLivros)
+routes.put("/livros/:id", LivroController.atualizarLivro)
+routes.delete("/livros/:id", LivroController.deletarLivro);
 
 export default routes;
