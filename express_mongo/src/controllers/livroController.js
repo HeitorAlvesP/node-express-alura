80643c83ae211942c7b40aa95da47c3f1358dd9d
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-02-21 12:10:11 -0300

    refactoring

--- express_mongo/src/controllers/livroController.js
+++ express_mongo/src/controllers/livroController.js
@@ -7,6 +7,16 @@ class LivroController {
         res.status(200).json(listaLivros)
     }
 
+    static async cadastrarLivros (req, res) {
+        try {
+            const novoLivro = await livro.create(req.body);
+            req.status(201).json({ message: "criado com sucesso", livro: novoLivro})
+        } catch (error) {
+            res.status(500).json({ message: `${error.message} - falha ao cadastrar o livro` })
+        }
+        
+    }
+
 };
 
 export default LivroController;
