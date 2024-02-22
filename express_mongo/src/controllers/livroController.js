e8649e328b992eae4d81ab808ac4233bf4729679
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-02-22 17:14:36 -0300

    conexão entre entidades

--- express_mongo/src/controllers/livroController.js
+++ express_mongo/src/controllers/livroController.js
@@ -1,3 +1,4 @@
+import { autor } from "../models/Autor.js";
 import livro from "../models/Livro.js";
 
 class LivroController {
@@ -12,13 +13,15 @@ class LivroController {
     }
 
     static async cadastrarLivros (req, res) {
+        const novoLivro = req.body;
         try {
-            const novoLivro = await livro.create(req.body);
-            res.status(201).json({ message: "criado com sucesso", livro: novoLivro})
+            const autorEncontrado = await autor.findById(novoLivro.autor);
+            const livroCompleto = { ...novoLivro, autor: { ...autorEncontrado._doc }};
+            const livroCriado = await livro.create(livroCompleto)
+            res.status(201).json({ message: "criado com sucesso", livro: livroCriado});
         } catch (error) {
             res.status(500).json({ message: `${error.message} - falha ao cadastrar o livro` })
         }
-        
     }
 
     static async buscarLivro (req, res) {
