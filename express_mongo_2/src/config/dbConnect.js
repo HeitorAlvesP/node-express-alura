ba98dc9574f044c9e733b7b17457b90b8d3dfb5c
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-02-28 14:34:04 -0300

    conexao com banco seguro

--- express_mongo_2/src/config/dbConnect.js
+++ express_mongo_2/src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
-mongoose.connect("mongodb+srv://heitor_dev:heitor_dev@clientes.xhdenmz.mongodb.net/");
+mongoose.connect(process.env.STRING_CONEXAO_DB);
 
 let db = mongoose.connection;
 
